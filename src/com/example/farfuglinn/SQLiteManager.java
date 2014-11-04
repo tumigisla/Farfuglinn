5d7420b7d70d2865a75f36c93ca9b89202c696db
tumsgis <tumsgis@gmail.com>
2014-11-04 12:47:17 +0000

    Sqlite manager, nota sem backup ef gögn skila sér ekki, og líka tþa. vera ekki að sækja gögn of oft. Hægt að geyma yourflights í þessu líka.

---
+++ src/com/example/farfuglinn/SQLiteManager.java
@@ -0,0 +1,188 @@
+/*
+ * Tumi Snær Gíslason
+ * November 4th 2014
+ * Creates and updates an sqlite db containing flights and departures.
+ * Parses JSONObject into the db.
+ */
+
+package com.example.farfuglinn;
+
+import android.database.sqlite.*;
+
+import android.content.*;
+import java.util.Objects;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+
+
+public class SQLiteManager extends SQLiteOpenHelper{
+
+	private static final String DB_NAME = "Flights";
+	private static final int DB_VERSION = 1;
+	
+	private static final String TAG_RESULTS = "results";
+	
+	// Database attributes
+	private static final String ATTR_DATE = "date";
+	private static final String ATTR_FLIGHTNUMBER = "flightNumber";
+	private static final String ATTR_AIRLINE = "airline";
+	private static final String ATTR_FROM = "fromPlace";
+	private static final String ATTR_PLANNEDARRIVAL = "plannedArrival";
+	private static final String ATTR_REALARRIVAL = "realArrival";
+	private static final String ATTR_STATUS = "status";
+	private static final String ATTR_TO = "toPlace";
+	
+	private static HashMap<String, String> dropTablesMap = new HashMap<>();
+	private static final String DROP_TABLES = "" +
+			"DROP TABLE IF EXISTS Departures;" +
+			"DROP TABLE IF EXISTS Arrivals;"
+		;	
+	
+	
+	
+	public SQLiteManager(Context context) {
+		super(context, DB_NAME, null, DB_VERSION);
+	}
+	
+	
+	
+	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(this.createTable("Departures"));
+		db.execSQL(this.createTable("Arrivals"));
+	}
+	
+	
+	
+	
+	
+	
+	
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		
+		// Tear down and build database again
+		db.execSQL(DROP_TABLES);
+		
+		this.onCreate(db);
+	}
+
+	
+	// Input: String jsonStr containing the JSON data
+	//			table is Departures/Arrivals
+	// Post: jsonStr -> JSONObject -> JSONArray -> this database 
+	public void jsonToDb(String jsonStr, String table) {
+		
+		dropTablesMap.put("Departures", "DROP TABLE IF EXISTS Departures;");
+		dropTablesMap.put("Arrivals", "DROP TABLE IF EXISTS Arrivals;");
+		
+		// Get reference to this (readable) database
+		SQLiteDatabase db = this.getReadableDatabase();
+		
+		db.execSQL(dropTablesMap.get(table));
+		db.execSQL(this.createTable(table));
+
+		db.close();
+		
+		
+		try {
+			JSONObject JSONObject = new JSONObject(jsonStr);
+			JSONArray jsonArray = JSONObject.getJSONArray(TAG_RESULTS);
+			
+			// Insert all the JSON strings into the database
+			// Open up database connection for each insertion to database because
+			// we might be inserting to both Departures and Arrivals at the
+			// same time, from separate threads.
+			for (int i = 0; i < jsonArray.length(); i++) {
+				
+				JSONObject object = jsonArray.getJSONObject(i);
+				
+				ContentValues values = new ContentValues();
+				values.put(ATTR_DATE, object.getString(ATTR_DATE));
+				values.put(ATTR_FLIGHTNUMBER, object.getString(ATTR_FLIGHTNUMBER));
+				values.put(ATTR_AIRLINE, object.getString(ATTR_AIRLINE));
+				values.put(ATTR_FROM, object.getString("from"));
+				values.put(ATTR_PLANNEDARRIVAL, object.getString(ATTR_PLANNEDARRIVAL));
+				values.put(ATTR_REALARRIVAL, object.getString(ATTR_REALARRIVAL));
+				values.put(ATTR_STATUS, object.getString(ATTR_STATUS));
+				values.put(ATTR_TO, object.getString("to"));
+				
+				db = this.getReadableDatabase();
+				db.insert(table, null, values); // null is nullColumnHack
+				db.close();
+			}
+		}
+		catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	/*
+	 * Input: String table -> "Departures"/"Arrivals"
+	 * Output: String with table declaration for Departures/Arrivals
+	 */
+	private String createTable(String table) {
+		
+		String dbStr = null;
+		
+		// Get current Unix epoch time and convert to String.
+				// long value in case of Y2038 problem, you never know...
+				long unixTimeStamp = System.currentTimeMillis() / 1000;
+				String unixTimeStampStr = Objects.toString(unixTimeStamp);
+				
+				// Create table departures
+				String CREATE_TABLE_DEPARTURES = "" +
+					"CREATE TABLE Departures (" +
+						"date varchar(7)," +
+						"flightNumber varchar(7)," +
+						"airline varchar(20)," +
+						"fromPlace varchar(20)," +
+						"plannedArrival varchar(5)," +
+						"realArrival varchar(5)," +
+						"status varchar(10)," +
+						"toPlace varchar(20)," +
+						"timeStamp INTEGER DEFAULT " + unixTimeStampStr + "," +
+						"PRIMARY KEY(flightNumber)" + // will be unique for each day at least
+					");";
+				
+				// Create table arrivals
+				String CREATE_TABLE_ARRIVALS = "" +
+					"CREATE TABLE Arrivals (" +
+						"date varchar(7)," +
+						"flightNumber varchar(7)," +
+						"airline varchar(20)," +
+						"fromPlace varchar(20)," +
+						"plannedArrival varchar(5)," +
+						"realArrival varchar(5)," +
+						"status varchar(10)," +
+						"toPlace varchar(20)," +
+						"timeStamp INTEGER DEFAULT " + unixTimeStampStr + "," +
+						"PRIMARY KEY(flightNumber)" + // will be unique for each day at least
+					");";
+		
+		
+		
+		
+		if (table.equals("Departures")) {
+			dbStr = CREATE_TABLE_DEPARTURES;
+		}
+		else if (table.equals("Arrivals")) {
+			dbStr = CREATE_TABLE_ARRIVALS;
+		}
+		
+		return dbStr;
+	}
+	
+	
+	
+}
