a4296b0bb49d215c922e2a3fd0c116f08cdd3e7a
tumsgis <tumsgis@gmail.com>
2014-11-26 21:12:40 +0000

    Klasar sem eru ekki í notkun merktir

---
+++ src/hbv1/farfuglinn/NOT_BEGIN_USED_getData_ellert.java
@@ -0,0 +1,70 @@
+/*  Ellert Finnbogi Eiríksson
+ *   12.10.2014
+ *   Takes in a url and returns a json string
+ *   
+ *   THIS CLASS IS NOT BEING USED FOR NOW
+ */
+
+
+package hbv1.farfuglinn;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+//import java.io.IOException;
+
+
+public class NOT_BEGIN_USED_getData_ellert {
+
+	public static String getFlightInfo(String url)
+	{
+
+		DefaultHttpClient   httpclient = new DefaultHttpClient(new BasicHttpParams());
+		//String url = "http://apis.is/flight?language=en&type=departures";
+
+		// Depends on your web service"
+		HttpPost httppost = new HttpPost(url);
+		httppost.setHeader("Content-type", "application/json");
+
+
+
+		InputStream inputStream = null;
+		String result = null;
+
+
+		try {
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity entity = response.getEntity();
+
+			inputStream = entity.getContent();
+			// json is UTF-8 by default
+			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+			StringBuilder sb = new StringBuilder();
+
+			String line = null;
+			while ((line = reader.readLine()) != null)
+			{
+				sb.append(line + "\n");
+			}
+			result = sb.toString();
+		} catch (Exception e) {
+			// Oops
+		}
+		finally {
+			try{if(inputStream != null) {
+				inputStream.close();
+			}}catch(Exception squish){}
+		}
+
+
+		return result;
+	}
+}
+
+
